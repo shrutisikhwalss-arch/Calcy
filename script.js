3460618b9290430326c70b5c997f75acd1389f49
Shruti Sikhwal <shrutisikhwalss@gmail.com>
2026-02-28 17:47:47 +0530

    Add functions for display interaction and calculation

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const display = document.getElementById("display");
+const clickSound = document.getElementById("clickSound");
+
+function press(value) {
+    clickSound.play();
+    display.value += value;
+}
+
+function clearDisplay() {
+    clickSound.play();
+    display.value = "";
+}
+
+function calculate() {
+    clickSound.play();
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "🌧️ Oops!";
+    }
+}
